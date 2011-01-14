71c33f1b42a6246dc4780e00b298d3ab5bb4ab07
Jeremy Shipman <jeremy@burnbright.net>
2011-01-14 00:44:25 +0000

    removed director callback

--- _config.php
+++ _config.php
@@ -4,7 +4,7 @@ Object::add_extension('SiteTree', 'FeedbackPageDecorator');
 /**
  * Register a callback function for all page views.
  */
-Director::add_callback(array("FeedbackPageDecorator","PreInitRequirements"));
+//Director::add_callback(array("FeedbackPageDecorator","PreInitRequirements"));
 
 
 ?>
